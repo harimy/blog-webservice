ce6804ce1b44205f94593fcf015906f617c27e0b
harim <harimlee1022@gmail.com>
2022-07-31 12:41:28 +0900

    Posts 도메인 설계 및 등록, 수정, 삭제 API 추가

---
+++ src/main/java/com/rimha/blog/service/posts/PostsService.java
@@ -0,0 +1,40 @@
+package com.rimha.blog.service.posts;
+
+import com.rimha.blog.domain.posts.Posts;
+import com.rimha.blog.domain.posts.PostsRepository;
+import com.rimha.blog.web.dto.PostsResponseDto;
+import com.rimha.blog.web.dto.PostsSaveRequestDto;
+import com.rimha.blog.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
+    }
+
+}
---
+++ src/main/java/com/rimha/blog/web/controller/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.rimha.blog.web.controller;
+
+import com.rimha.blog.service.posts.PostsService;
+import com.rimha.blog.web.dto.PostsResponseDto;
+import com.rimha.blog.web.dto.PostsSaveRequestDto;
+import com.rimha.blog.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
