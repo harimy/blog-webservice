fbae1ddac36e664e8a41f9c373cedd7f305b8d66
harim <harimlee1022@gmail.com>
2022-08-11 21:15:01 +0900

    직렬화를 구현하지 않았따는 에러 해결을 위해 implements Serializable 추가

--- src/main/java/com/rimha/blog/config/auth/dto/SessionUser.java
+++ src/main/java/com/rimha/blog/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.rimha.blog.config.auth.dto;
 import com.rimha.blog.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
