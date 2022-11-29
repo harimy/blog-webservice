5d094dfdb9f6c2528cfe436d35862df87dcfc4a3
harim <harimlee1022@gmail.com>
2022-11-30 01:27:44 +0900

    profile api 추가

--- src/main/java/com/rimha/blog/config/auth/SecurityConfig.java
+++ src/main/java/com/rimha/blog/config/auth/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**",
-                        "/js/**", "/h2-console/**").permitAll()
+                        "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
